a7da25bde51adef0eaa2b4b9003c8290fdfdd35d
william hunter <hunterwilliam795@gmail.com>
2023-11-23 00:00:56 -0500

    Added search that prints to the console

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+var input = document.getElementById('searchInput').value.trim()
+var button = document.getElementById('searchButton')
 function searchButton() {
-var input = document.getElementById('searchInput').value.toLowerCase()
-var searchOutput = search(input)
-
-displayResults(results);
+    movieSearch()
 }
+
+function movieSearch() {
+    var Apilink = "http://www.omdbapi.com/?t=" + input + "&apikey=8828c04b"
+    fetch(Apilink)
+        .then(function (response) {
+            return response.json()            
+        }
+        )
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log('Unable to connect to API', error)
+        })
+    }
+    
