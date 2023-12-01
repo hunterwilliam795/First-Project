69d76d3a55119f126e3bbedbb0cf85096371b7f4
william hunter <hunterwilliam795@gmail.com>
2023-11-30 19:24:28 -0500

    attempted fixes

--- script.js
+++ script.js
@@ -50,6 +50,10 @@ function callAnotherApi(imdbID) {
 
 
 let player;
+var tag = document.createElement('script')
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
   function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
