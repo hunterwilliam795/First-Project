d08ab4bdf7db0439c7f48ffd19f50355053d6730
william hunter <hunterwilliam795@gmail.com>
2023-12-06 20:20:14 -0500

    reworked to display multiple movies

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+var displayArea = document.getElementById('displayArea');
+
 var input = document.getElementById('searchInput')
+
 var button = document.getElementById('searchButton')
 function searchButton() {
   movieSearch()
@@ -21,12 +24,34 @@ function movieSearch() {
       console.log(data);
 
       if (data.Search) {
-        
-        var imdbIDs = data.Search.map(function (movie) {
-          return movie.imdbID;
-        });
+        displayArea.innerHTML = ''
+        for (var i = 0; i < data.Search.length; i++) {
+          posterElement = document.createElement('img');
+          posterElement.src = data.Search[i].Poster;
+          posterElement.alt = 'Movie Poster';
+          posterElement.style.marginRight = '10px';
+          posterElement.style.width = "150px";
+          titleElement = document.createElement('h3');
+          titleElement.textContent = 'Title: ' + data.Search[i].Title
+          yearElement = document.createElement('p');
+          yearElement.textContent = 'Title: ' + data.Search[i].Year
+          button = document.createElement('button')
+          button.setAttribute('data-id',data.Search[i].imdbID)
+          breakElement = document.createElement('br')
+          breakElement.setAttribute('break','5')
+          // button.textContent('More Info')
+          displayArea.append(posterElement,titleElement,yearElement,button,breakElement)
+          
+        }
+        // var imdbIDs = data.Search.map(function (movie) {
+        //   return movie.imdbID;
+        // });
 
-        getMovieDetails(imdbIDs);
+
+
+
+
+
       } else {
         console.log('No search results found');
       }
@@ -35,7 +60,7 @@ function movieSearch() {
       console.log('Unable to connect to API', error);
     });
 }
-
+var movieDetailsArray = []
 function getMovieDetails(imdbIDs) {
   imdbIDs.forEach(function (imdbID) {
     var detailApiLink = "https://www.omdbapi.com/?i=" + imdbID + "&apikey=8828c04b";
@@ -44,16 +69,15 @@ function getMovieDetails(imdbIDs) {
         return response.json();
       })
       .then(function (movieDetails) {
-        console.log('Movie Details:', movieDetails);
-        displayMovieDetails(movieDetails);
+        movieDetailsArray.push(movieDetails);
+        displaydata(movieDetailsArray);
+        console.log('Movie Details:', movieDetailsArray);
       })
       .catch(function (error) {
         console.log('Unable to fetch movie details', error);
       });
   });
 }
-
-
 
 
 function callAnotherApi(imdbID) {
@@ -91,7 +115,6 @@ function getlink(data) {
   playerElement.style.display = "absolute"
   iframeElement.src = `https://www.youtube.com/embed/${videoId}`;
 
-  ;
 
   playerElement.appendChild(iframeElement);
 
@@ -100,7 +123,7 @@ function getlink(data) {
 
 function extractVideoId(url) {
   // Regular expression to match YouTube video ID in various URL formats
-  var regExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  var regExp = '/^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/';
   var match = url.match(regExp);
 
 
@@ -110,77 +133,79 @@ function extractVideoId(url) {
 }
 
 function displaydata(data) {
-  displayArea = document.getElementById('displayArea');
-  displayArea.innerHTML = '';
+  for (i = 0; i < movieDetailsArray.length; i++) {
+
+    displayArea.innerHTML = '';
+
+    // Container for Poster and Text
+    container = document.createElement('div');
+    container.style.display = 'flex';
 
 
-  // Container for Poster and Text
-  container = document.createElement('div');
-  container.style.display = 'flex';
+
+    // Poster Element
+    posterElement = document.createElement('img');
+    posterElement.src = movieDetailsArray.Poster;
+    posterElement.alt = 'Movie Poster';
+    posterElement.style.marginRight = '10px';
+    posterElement.style.width = "150px";
+    container.appendChild(posterElement);
+
+    // Text Elements
+    textContainer = document.createElement('div');
+    textContainer.style.maxWidth = '600px';
+
+
+    // Title Element
+    titleElement = document.createElement('h3');
+    titleElement.textContent = 'Title: ' + movieDetailsArray.Title
+    textContainer.appendChild(titleElement);
+
+    // Runtime Element
+    runtimeElement = document.createElement('h2');
+    runtimeElement.textContent = 'Runtime: ' + movieDetailsArray.Runtime;
+    textContainer.appendChild(runtimeElement);
+
+    // Release Date Element
+    releasedateElement = document.createElement('h2');
+    releasedateElement.textContent = 'Release Date: ' + movieDetailsArray.Released;
+    textContainer.appendChild(releasedateElement);
+
+    // Actors
+    actorsElement = document.createElement('h2');
+    actorsElement.textContent = 'Actors: ' + movieDetailsArray.Actors;
+    textContainer.appendChild(actorsElement);
+    container.appendChild(textContainer);
+    displayArea.appendChild(container);
+
+    // Plot
+    plotElement = document.createElement('p');
+    plotElement.textContent = 'Plot: ' + movieDetailsArray.Plot;
+    textContainer.appendChild(plotElement);
+
+    // MetaScore
+    metaElement = document.createElement('h2');
+    metaElement.textContent = 'Health Meter: ' + movieDetailsArray.Metascore;
+    textContainer.appendChild(metaElement);
 
 
 
-  // Poster Element
-  posterElement = document.createElement('img');
-  posterElement.src = data.Poster;
-  posterElement.alt = 'Movie Poster';
-  posterElement.style.marginRight = '10px';
-  posterElement.style.width = "150px";
-  container.appendChild(posterElement);
+    callAnotherApi(data.imdbID);
+    // // Trailer
+    if (data.trailer) {
+      var trailerContainer = document.createElement('div');
+      trailerContainer.style.marginTop = '20px';
 
-  // Text Elements
-  textContainer = document.createElement('div');
-  textContainer.style.maxWidth = '600px';
+      iframeElement = document.createElement('iframe');
+      iframeElement.width = "420";
+      iframeElement.height = "315";
+      iframeElement.src = `https://www.youtube.com/embed/${videoId}`;
+      playerElement = document.getElementById('player');
+      playerElement.appendChild(iframeElement);
 
-
-  // Title Element
-  titleElement = document.createElement('h3');
-  titleElement.textContent = 'Title: ' + data.Title;
-  textContainer.appendChild(titleElement);
-
-  // Runtime Element
-  runtimeElement = document.createElement('h2');
-  runtimeElement.textContent = 'Runtime: ' + data.Runtime;
-  textContainer.appendChild(runtimeElement);
-
-  // Release Date Element
-  releasedateElement = document.createElement('h2');
-  releasedateElement.textContent = 'Release Date: ' + data.Released;
-  textContainer.appendChild(releasedateElement);
-
-  // Actors
-  actorsElement = document.createElement('h2');
-  actorsElement.textContent = 'Actors: ' + data.Actors;
-  textContainer.appendChild(actorsElement);
-  container.appendChild(textContainer);
-  displayArea.appendChild(container);
-
-  // Plot
-  plotElement = document.createElement('p');
-  plotElement.textContent = 'Plot: ' + data.Plot;
-  textContainer.appendChild(plotElement);
-
-  // MetaScore
-  metaElement = document.createElement('h2');
-  metaElement.textContent = 'Health Meter: ' + data.Metascore;
-  textContainer.appendChild(metaElement);
-
-
-  callAnotherApi(data.imdbID);
-  // // Trailer
-  if (data.trailer) {
-    var trailerContainer = document.createElement('div');
-    trailerContainer.style.marginTop = '20px';
-
-    iframeElement = document.createElement('iframe');
-    iframeElement.width = "420";
-    iframeElement.height = "315";
-    iframeElement.src = `https://www.youtube.com/embed/${videoId}`;
-    playerElement = document.getElementById('player');
-    playerElement.appendChild(iframeElement);
-
-    trailerContainer.appendChild(trailerElement);
-    textContainer.appendChild(trailerContainer);
+      trailerContainer.appendChild(trailerElement);
+      textContainer.appendChild(trailerContainer);
+    }
   }
 
 }
